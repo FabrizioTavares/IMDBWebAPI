6f66679d58bc97830a9279808ccf1eaab5cc3d3f
Fabrizio <fab2tav@gmail.com>
2022-12-27 10:47:12 -0300

    Add admin validators

---
+++ Services/Validation/Admin/CreateAdminDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using Domain.DTOs.AdminDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Validation.Admin
+{
+    public class CreateAdminDTOValidator : AbstractValidator<CreateAdminDTO>
+    {
+        public CreateAdminDTOValidator()
+        {
+            RuleFor(a => a.Username).NotEmpty().WithMessage("Username is required");
+            RuleFor(a => a.Password).NotEmpty().WithMessage("Password is required");
+            RuleFor(a => a.Password).MinimumLength(8).WithMessage("Password must be at least 8 characters long");
+            RuleFor(a => a.Hierarchy).GreaterThan(-1).WithMessage("Hierarchy must be greater than -1");
+        }
+    }
+}
---
+++ Services/Validation/Admin/UpdateAdminDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using Domain.DTOs.AdminDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Validation.Admin
+{
+    public class UpdateAdminDTOValidator : AbstractValidator<UpdateAdminDTO>
+    {
+        public UpdateAdminDTOValidator()
+        {
+            RuleFor(a => a.Username).NotEmpty().WithMessage("Username is required");
+            RuleFor(a => a.NewPassword).MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long")
+                .When(a => a.NewPassword != null);
+        }
+    }
+}
