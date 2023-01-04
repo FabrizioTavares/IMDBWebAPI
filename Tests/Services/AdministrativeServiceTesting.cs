13904787654db9a0d195331c8b8b4061c8b6d31c
Fabrizio <fab2tav@gmail.com>
2023-01-04 11:38:40 -0300

    Rename tests to match tested methods

--- Tests/Services/AdministrativeServiceTesting.cs
+++ Tests/Services/AdministrativeServiceTesting.cs
@@ -371,7 +371,7 @@ public class AdministrativeServiceTesting
     }
     
     [Fact]
-    public async void Update_WhenUpdatingOneSelfWithValidPassword_ShouldReturnSuccess()
+    public async void UpdateAdmin_WhenUpdatingOneSelfWithValidPassword_ShouldReturnSuccess()
     {
         // Arrange
         var loggedAdmin = _context[0];
